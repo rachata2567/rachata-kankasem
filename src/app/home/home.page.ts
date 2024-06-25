7a0331072a41c446cdc3f0710999da904a452d7c
rachata2567 <tuynuy.kankasem@gmail.com>
2024-06-25 20:16:22 +0700

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,31 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  weight ? : number;
+  hight ? : number;
+  result ? : number;
+  result_text : string ="";
   constructor() {}
 
+  onCal(){
+    console.log("w : " + this.weight)
+    console.log("h : " + this.hight)
+    if(this.weight == undefined || this.hight == undefined){
+      alert("กรุณากรอกข้อมูลให้ครบถ้วน")
+      return;
+    }
+    var hi = this.hight / 100 ;
+    this.result = this.weight / (hi * hi)
+    console.log('result' + this.result)
+
+    if(this.result < 18.5){
+       this.result_text ="ผอมเกินไป";
+    }else if(this.result >= 18.6 && this.result <= 24.9){
+      this.result_text ="อยู่เกณฑ์ปกติ เหมาะสม";
+    }else if(this.result >= 25 && this.result <= 30.9){
+      this.result_text ="อ้วน";
+    }else {
+this.result_text = "อ้วนมาก";
+    }
+  }
 }
